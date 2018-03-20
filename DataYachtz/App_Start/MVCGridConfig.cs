5b73f086af17a1022ec3ddad774c08a56c08e406
Leon Grund <leoncapgrund@gmail.com>
2018-03-20 02:07:21 -0500

    added field functionality

--- DataYachtz/App_Start/MVCGridConfig.cs
+++ DataYachtz/App_Start/MVCGridConfig.cs
@@ -21,26 +21,43 @@ namespace DataYachtz
             
             MVCGridDefinitionTable.Add("UsageExample", new MVCGridBuilder<UserCSVModels>()
                 .WithAuthorizationType(AuthorizationType.AllowAnonymous)
+                .WithSorting(sorting: true, defaultSortColumn: "Id", defaultSortDirection: SortDirection.Dsc)
+                .WithPaging(paging: true, itemsPerPage: 10, allowChangePageSize: true, maxItemsPerPage: 100)
+                .WithAdditionalQueryOptionNames("search")
                 .AddColumns(cols =>
                 {
-                    // Add your columns here
+                    cols.Add("Id").WithValueExpression((p, c) => c.UrlHelper.Action("detail", "demo", new { id = p.Id }))
+                        .WithValueTemplate("<a href='{Value}'>{Model.Id}</a>", false)
+                        .WithPlainTextValueExpression(p => p.Id.ToString());
                     cols.Add().WithColumnName("Email")
                         .WithHeaderText("EMAIL")
+                        .WithVisibility(true, true)
                         .WithValueExpression(i => i.Email); // use the Value Expression to return the cell text for this column
                     cols.Add().WithColumnName("CSV")
                         .WithHeaderText("CSV")
-                         .WithValueExpression(i => i.Email);
+                        .WithVisibility(true, true)
+                        .WithValueExpression(i => i.Email);
                 })
                 .WithRetrieveDataMethod((context) =>
                 {
                     // Query your data here. Obey Ordering, paging and filtering parameters given in the context.QueryOptions.
                     // Use Entity Framework, a module from your IoC Container, or any other method.
                     // Return QueryResult object containing IEnumerable<YouModelItem>
+                    var options = context.QueryOptions;
+                    int totalRecords;
+                    var repo = HomeController._dbContext.UserCSVs.ToList();   //DependencyResolver.Current.GetService<UserCSVModels>();
+                    string globalSearch = options.GetAdditionalQueryOptionString("search");
+                    string sortColumn = options.GetSortColumnData<string>();
+                    var items = repo;//.GetData(out totalRecords, globalSearch, options.GetLimitOffset(), options.GetLimitRowcount(),
+                        //sortColumn, options.SortDirection == SortDirection.Dsc);
                     
                     return new QueryResult<UserCSVModels>()
                     {
-                        Items = HomeController._dbContext.UserCSVs.ToList(),
-                        TotalRecords = 0 // if paging is enabled, return the total number of records of all pages
+                        //Items = HomeController._dbContext.UserCSVs.ToList(),
+                        Items = items,
+                        TotalRecords = 3 //totalRecords
+
+                        //TotalRecords = 0 // if paging is enabled, return the total number of records of all pages
                     };
 
                 })
