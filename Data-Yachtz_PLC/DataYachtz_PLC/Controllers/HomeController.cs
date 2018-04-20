8963c2ee841af5e4ff2ce3c8a0425379fa449fc9
Leon Grund <leoncapgrund@gmail.com>
2018-04-20 15:46:55 -0500

    clientGrid

--- Data-Yachtz_PLC/DataYachtz_PLC/Controllers/HomeController.cs
+++ Data-Yachtz_PLC/DataYachtz_PLC/Controllers/HomeController.cs
@@ -11,6 +11,54 @@ namespace DataYachtz_PLC.Controllers
 {
     public class HomeController : Controller
     {
+
+        public CsvModel Model;
+
+
+        private bool ContainsTime(DateTime modelDate, string searchDate)
+        {
+            bool equal = false;
+            var mDate = Regex.Split(modelDate.ToString().Trim(), "/:");
+            var sDate = searchDate.ToString().Trim().Split('-');
+
+            foreach (var e in mDate)
+            {
+                foreach (var s in sDate)
+                {
+                    if (e.Contains(s)) { return true; break; }
+                }
+                if (equal) { break; }
+            }
+            return equal;
+        }
+
+        public HomeController()
+        {
+            Model = new CsvModel();
+            List<ItemMasterModel> Models;
+            using (var entities = new ApplicationDbContext())
+            {
+                Models = entities.ItemMasters.Select(X => X).ToList();
+
+                foreach (var model in Models)
+                {
+                    var cell = new Cell(Model.ColumnNames);
+                    cell.SetCellInfo(Model.ColumnNames[0], model.PartNumber);
+                    cell.SetCellInfo(Model.ColumnNames[1], model.Specification);
+                    cell.SetCellInfo(Model.ColumnNames[2], model.Description);
+                    cell.SetCellInfo(Model.ColumnNames[3], model.CreatedDate.ToString());
+                    Model.AddCell(cell);
+                }
+            }
+
+        }
+
+        public ActionResult JQDataTableClients()
+        {
+
+            return View(Model);
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,9 +68,7 @@ namespace DataYachtz_PLC.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(Model);
         }
 
 
@@ -34,29 +80,10 @@ namespace DataYachtz_PLC.Controllers
 
 
 
-
-
-        private bool ContainsTime(DateTime modelDate, string searchDate)
-        {
-            bool equal = false;
-            var mDate = Regex.Split(modelDate.ToString().Trim(), "/:");
-            var sDate = searchDate.ToString().Trim().Split('-');
-
-            foreach(var e in mDate)
-            {
-                foreach (var s in sDate)
-                {
-                    if (e.Contains(s)) { return true; break; }
-                }
-                if (equal) { break; }
-            }
-            return equal;
-        }
-
         public ActionResult Index()
         {
             //var items = GetItems("", "", "");
-            return View();
+            return View(Model);
         }
 
         // With server side search Sorting
@@ -75,45 +102,68 @@ namespace DataYachtz_PLC.Controllers
 
             var SortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][data]").FirstOrDefault();
             var SortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
-            
-            
-            // SEARCH INPUTS
 
+
+            // SEARCH INPUTS
+            var SearchInput = new String[Model.GetSize()+1];
+            for (int i = 0; i < Model.GetSize()-1; i++)
+            {
+                SearchInput[i] = Request.Form.GetValues("columns[" + i.ToString() + "][search][value]").FirstOrDefault();
+            }
+            SearchInput[Model.GetSize()] = Request.Form.GetValues("search[value]").FirstOrDefault();
+
+            /*
             var PartNo = Request.Form.GetValues("columns[0][search][value]").FirstOrDefault();
             var Spec = Request.Form.GetValues("columns[1][search][value]").FirstOrDefault();
             var Desc = Request.Form.GetValues("columns[2][search][value]").FirstOrDefault();
-            var Date = Request.Form.GetValues("columns[3][search][value]").FirstOrDefault();
+            //var Date = Request.Form.GetValues("columns[3][search][value]").FirstOrDefault();
             var Global = Request.Form.GetValues("search[value]").FirstOrDefault();
+            */
 
             int PageSize = Length != null ? Convert.ToInt32(Length) : 0;
             int Skip = Start != null ? Convert.ToInt32(Start) : 0;
-            int TotalRecords = 0;
+            int TotalRecords = 4; //Model.Data.Count();
 
             List<ItemMasterModel> Models;
             using (var entities = new ApplicationDbContext())
             {
                 Models = entities.ItemMasters.Select( X => X).ToList();
+                
+                foreach (var model in Models)
+                {
+                    var cell = new Cell(Model.ColumnNames);
+                    cell.SetCellInfo(Model.ColumnNames[0], model.PartNumber);
+                    cell.SetCellInfo(Model.ColumnNames[1], model.Specification);
+                    cell.SetCellInfo(Model.ColumnNames[2], model.Description);
+                    Model.AddCell(cell);
+                }
             }
 
+
+            /*
             var Items = (from a in Models
-                             .Where(x => 
-                             (x.Description.ToLower().Contains(Desc.ToLower()) && x.Specification.ToLower().Contains(Spec.ToLower()) && 
-                             x.PartNumber.ToLower().Contains(PartNo.ToLower()) && ContainsTime(x.CreatedDate, Date)) &&
+                           .Where(x =>
+                           (x.Description.ToLower().Contains(Desc.ToLower()) && x.Specification.ToLower().Contains(Spec.ToLower()) &&
+                           x.PartNumber.ToLower().Contains(PartNo.ToLower()) && ContainsTime(x.CreatedDate, "")) && //Date
 
-                             (x.Description.ToLower().Contains(Global.ToLower()) || x.Specification.ToLower().Contains(Global.ToLower()) || 
-                             x.PartNumber.ToLower().Contains(Global.ToLower())))
+                           (x.Description.ToLower().Contains(Global.ToLower()) || x.Specification.ToLower().Contains(Global.ToLower()) ||
+                           x.PartNumber.ToLower().Contains(Global.ToLower())))
 
 
-                             .Take(4000)
-                             select new ItemMasterModel
-                             {
-                                 PartNumber = a.PartNumber.Trim(),
-                                 Specification = a.Specification.Trim(),
-                                 Description = a.Description.Trim(),
-                                 CreatedDate = a.CreatedDate
-                             });
-                
+                           .Take(4000)
+                         select new ItemMasterModel
+                         {
+                             PartNumber = a.PartNumber.Trim(),
+                             Specification = a.Specification.Trim(),
+                             Description = a.Description.Trim(),
+                             CreatedDate = a.CreatedDate
+                         }); 
             
+            
+
+
+            
+
 
             if (!(string.IsNullOrEmpty(SortColumn) && string.IsNullOrEmpty(SortColumnDir)))
             { //Items = Items.OrderBy(SortColumn + " " + SortColumnDir);
@@ -145,8 +195,10 @@ namespace DataYachtz_PLC.Controllers
 
             TotalRecords = Items.ToList().Count();
             var NewItems = Items.Skip(Skip).Take(PageSize).ToList();
+            */
+            //List<Cell> NewItems = Model.Data;
 
-            return Json(new { draw = Draw, recordsFiltered = TotalRecords, recordsTotal = TotalRecords, data = NewItems }, JsonRequestBehavior.AllowGet);
+            return Json(new { draw = Draw, recordsFiltered = TotalRecords, recordsTotal = TotalRecords, data = Model.Data }, JsonRequestBehavior.AllowGet);
         }
 
 
